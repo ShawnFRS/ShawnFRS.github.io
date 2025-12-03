fb6b180014f78d30871c2e93ba3e98c2ff1e4e4e
ShawnFRS <alex.ffierro@gmail.com>
2025-12-02 23:05:08 -0300

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,6 +1,9 @@
-const ctx = document.getElementById("forecastChart");
+/* =============================
+   Gráfico principal (Forecast)
+============================= */
+const ctx1 = document.getElementById("forecastChart");
 
-new Chart(ctx, {
+new Chart(ctx1, {
     type: "line",
     data: {
         labels: ["W1", "W2", "W3", "W4", "W5", "W6", "W7"],
@@ -9,8 +12,35 @@ new Chart(ctx, {
             data: [5, 10, 85, 60, 45, 25, 10],
             borderColor: "#072c3f",
             borderWidth: 3,
-            pointRadius: 0,
-            tension: 0.3
+            pointRadius: 3,
+            tension: 0.35
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: { legend: { display: false }},
+        scales: {
+            y: { beginAtZero: true }
+        },
+        animations: {
+            tension: { duration: 900, easing: "easeOutBounce" }
+        }
+    }
+});
+
+/* =============================
+   Segundo gráfico (Desviaciones)
+============================= */
+const ctx2 = document.getElementById("desviacionChart");
+
+new Chart(ctx2, {
+    type: "bar",
+    data: {
+        labels: ["Mes 1", "Mes 2", "Mes 3"],
+        datasets: [{
+            label: "% Desviación",
+            data: [18, 7, -5],
+            backgroundColor: ["#f55b5b", "#ffc847", "#43c16f"]
         }]
     },
     options: {
@@ -21,6 +51,7 @@ new Chart(ctx, {
         }
     }
 });
+
 
 
 
